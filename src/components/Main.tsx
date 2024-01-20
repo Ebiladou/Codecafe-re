d2805961a13fc4277fb9399a43232c90c0f71500
Ebiladou <oyinebiladouomemu@gmail.com>
2024-01-20 16:21:08 +0100

    fixed section layout

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -10,33 +10,42 @@ function Main() {
         </p>
       </div>
 
-      <div className="flex-contain">
-        <div className="btns">
-            <button className="btn-one">Product Design</button>
-            <button className="btn-two">Technical Writing</button>
-            <button className="btn-three">Websites/Landing Pages</button>
-            <button className="btn-four">E-commerce Stores</button>
-            <button className="btn-five">Software consultancy</button>
+      <div className="service-contain">
+        <div className="service-cardOne">
+           <button className="btn-one">Product Design</button>
+          <div className="card-desOne">
+          <h1>Product Design</h1>
+                <p>With a client-centric approach, our team of experts is ready to deliver pixel-perfect designs for your use cases.</p>
+          </div>
         </div>
 
-        <div className="cards">
-            <div className="card-one">
-                <h1>Product Design</h1>
-                <p>With a client-centric approach, our team of experts is ready to deliver pixel-perfect designs for your use cases.</p>
-            </div>
-            <div className="card-two">
+        <div className="service-cardTwo">
+          <button className="btn-two">Technical Writing</button>
+          <div className="card-desTwo">
                 <h1>Technical Writing</h1>
                 <p>Work with our team of technical writing experts to attend to your technical writing needs; planning, content strategy, and improvement of existing technical documents.</p>
             </div>
-            <div className="card-three">
+        </div>
+
+        <div className="service-cardThree">
+          <button className="btn-three">Websites/Landing Pages</button>
+          <div className="card-desThree">
                 <h1>Websites/Landing Pages</h1>
                 <p>Upgrade your brand by working with our expert-led team to build your websites and landing pages.</p>
             </div>
-            <div className="card-four">
+        </div>
+
+        <div className="service-cardFour">
+          <button className="btn-four">E-commerce Stores</button>
+          <div className="card-desFour">
                 <h1>E-commerce Stores</h1>
                 <p>Upgrade your brand by working with our expert-led team to build your websites and landing pages.</p>
             </div>
-            <div className="card-five">
+        </div>
+
+        <div className="service-cardFive">
+          <button className="btn-five">Software consultancy</button>
+          <div className="card-desFive">
                 <h1>Software Consultancy</h1>
                 <p>Engage our team of experts to discuss general software needs not limited to software evaluation and assessment, technical content auditing, and general software contracts.</p>
             </div>
