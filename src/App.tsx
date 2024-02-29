9509c65bcdc5fcc478ed8e94fd65400597da10ea
Ebiladou <oyinebiladouomemu@gmail.com>
2024-03-01 00:00:20 +0100

    fixed path

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import Layout from "./components/Layout"
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
 
 function App() {
+  const location = useLocation();
 
   return (
     <>
-    <Routes>
+    <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Layout />} />
         <Route path="/privacy" element={<Privacy/>} />
         <Route path="/terms" element={<Terms/>}/>
