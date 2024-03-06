58042e35c2bf785abf1095dd0f5c9d9f3d297e61
Ebiladou <oyinebiladouomemu@gmail.com>
2024-03-06 23:21:31 +0100

    added geist package

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Layout from "./components/Layout"
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import 'non.geist';
 import Privacy from "./pages/Privacy";
 import Terms from "./pages/Terms";
 
 function App() {
   const location = useLocation();
-  console.log(location);
 
   return (
     <>
